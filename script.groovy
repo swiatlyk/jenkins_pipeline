d73ddff1385604f5cf1eff9ef8ba785aa36c43f0
Krzysztof Światły <swiatlyk@gmail.com>
2023-10-29 00:37:56 +0200

    Script adapted

--- script.groovy
+++ script.groovy
@@ -1,5 +1,5 @@
 def checkoutApplication() {
-    git credentialsId: 'id_of_stored_credential_in_jenkins_server', url: 'ssh://git@address_of_the_repository/name_of_repository.git'
+    git credentialsId: 'token for jenkins test buil', url: 'https://github.com/swiatlyk/jenkins_pipeline.git'
 }
 
 def precondition() {
@@ -16,30 +16,30 @@ def application_exe() {
 
 def copySourceDestination(){
     //bat("xcopy %SOURCE% %DESTINATION% /s /e /y")
-    bat returnStatus: true, script: '''if [%SOURCE%] == [] goto EMPTYS
-    if [%DESTINATION%] == [] goto EMPTYD
+    // bat returnStatus: true, script: '''if [%SOURCE%] == [] goto EMPTYS
+    // if [%DESTINATION%] == [] goto EMPTYD
 
-    xcopy %SOURCE% %DESTINATION% /s /e /y
+    // xcopy %SOURCE% %DESTINATION% /s /e /y
     
-    cd /d %DESTINATION%\\
+    // cd /d %DESTINATION%\\
     
-    if "%FRUP%" == "" goto END
+    // if "%FRUP%" == "" goto END
     
-    if "%FRUP%" == "NTG6FRUP1" (
-      echo "Nothing to do"
-    )
+    // if "%FRUP%" == "NTG6FRUP1" (
+    //   echo "Nothing to do"
+    // )
     
-    goto END
+    // goto END
     
-    :EMPTYS
-    echo "Empty source folder!"
-    exit /b 11
+    // :EMPTYS
+    // echo "Empty source folder!"
+    // exit /b 11
     
-    :EMPTYD
-    echo "Empty destination folder!"
-    exit /b 11
+    // :EMPTYD
+    // echo "Empty destination folder!"
+    // exit /b 11
     
-    :END
-    echo "End of file"'''
+    // :END
+    // echo "End of file"'''
 }
 return this
